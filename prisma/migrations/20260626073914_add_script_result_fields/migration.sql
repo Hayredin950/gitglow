ab72109ea46576bac5ab569cf398e191f29919c1
Hayredin950 <hayredin.950@gmail.com>
2026-06-26 10:40:51 +0300

    fix: remove DROP COLUMN for errorMessage from migration

--- prisma/migrations/20260626073914_add_script_result_fields/migration.sql
+++ prisma/migrations/20260626073914_add_script_result_fields/migration.sql
@@ -1,13 +1,11 @@
 /*
   Warnings:
 
-  - You are about to drop the column `errorMessage` on the `Polish` table. All the data in the column will be lost.
-
 */
 -- AlterEnum
 ALTER TYPE "PolishStatus" ADD VALUE 'READY';
 
 -- AlterTable
-ALTER TABLE "Polish" DROP COLUMN "errorMessage",
+ALTER TABLE "Polish"
 ADD COLUMN     "executionScript" TEXT,
 ADD COLUMN     "scriptResult" JSONB;
